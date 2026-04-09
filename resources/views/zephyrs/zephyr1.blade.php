dd46f0c7e92a3e5de5c1b6b668e210f5a77a9a34
hmzah <hmzah.liaqat@gmail.com>
2026-04-09 21:35:25 +0500

    fix/favicon

--- resources/views/zephyrs/zephyr1.blade.php
+++ resources/views/zephyrs/zephyr1.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">  
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Artboard 1</title>
+  <title>AminahInBloom</title>
+  <link rel="icon" type="image/png" href="/favicon.png">
   @vite(['resources/css/zephyr.css'])
 </head>
 <body>
